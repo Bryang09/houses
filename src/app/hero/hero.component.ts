1dcdd237edcc664368efe4dcd7fe9d02fa133428
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-18 14:21:21 -0600

    Hero Section Completed

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-hero',
-  templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  selector: "app-hero",
+  templateUrl: "./hero.component.html",
+  styleUrls: ["./hero.component.scss"]
 })
 export class HeroComponent implements OnInit {
+  heart: boolean = false;
+  share: boolean = false;
+  class: string = "far fa-heart";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onHeart() {
+    this.heart = !this.heart;
+    console.log(this.heart);
+
+    this.class = this.heart ? "fas fa-heart" : "far fa-heart";
   }
-
+  onShare() {
+    this.share = !this.share;
+    console.log(this.share);
+  }
 }
