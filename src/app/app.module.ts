f7ef0ee38110a28e77e12d605b172a72e3f7c40d
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-18 15:48:47 -0600

    Listings Like Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { config } from "../environments/environment";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeroComponent } from "./hero/hero.component";
 import { NavComponent } from "./nav/nav.component";
-import { TitleComponent } from './hero/title/title.component';
-import { ModalComponent } from './hero/modal/modal.component';
+import { TitleComponent } from "./hero/title/title.component";
+import { ModalComponent } from "./hero/modal/modal.component";
+import { Section1Component } from "./section1/section1.component";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "angularfire2/firestore";
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, NavComponent, TitleComponent, ModalComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    NavComponent,
+    TitleComponent,
+    ModalComponent,
+    Section1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
